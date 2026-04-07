3b01035c6cb42d89e225c77ed7e52511cca3b651
Paul Hazelton <hazeltonspaul@gmail.com>
2026-04-07 19:14:08 -0400

    Clean up

--- background.js
+++ background.js
@@ -3,31 +3,15 @@ chrome.runtime.onMessage.addListener((request) => {
 	if (request.action !== "notify")
 		return;
 
-	const platform = navigator.userAgentData.platform;
+	// console.log(`Sending message: ${request.text}`);
 
-	// Handle windows notification
-	// if (platform == "Windows" || platform == "Linux") {
-	if (platform == "Windows") {
-		// This uses chrome.notifications.create
-		console.log(`Sending message: ${request.text}`);
-		chrome.notifications.create({
-			type: "basic",
-			iconUrl: "icon.png",
-			title: "Message from Tester",
-			message: request.text
-		});
-		// Uncomment this for extra diagnostics if the messages don't come through.
-		// }, (notificationId) => {
-		// 	console.log("Notification created:", notificationId);
-		// 	console.log("Last error:", chrome.runtime.lastError);
-		// });
-	}
-	// Handle Linux/Arch/Omarchy notification using `notify-send`
-	// else if (platform == "Linux") {
-	else {
-		// This connects to the python script for linux notify-send
-		const port = chrome.runtime.connectNative('com.zoom.logger');
-		port.postMessage({ text: request.text });
-	}
-	// TODO, add proper else if for checking linux? Also check mac?
+	chrome.notifications.create({
+		type: "basic",
+		iconUrl: "icon.png",
+		title: "Message from Tester",
+		message: request.text
+	}, (notificationId) => {
+		// Wait some time, then call clear
+		setTimeout(() => { chrome.notifications.clear(notificationId) }, 5000);
+	});
 });
